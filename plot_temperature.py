4b08b5f4823b10b0364bb362178f37a890dba6a7
igrankinaUBx <irina.grankina@u-bordeaux.fr>
2025-08-25 16:37:49 +0200

    Update plot_temperature.py

--- plot_temperature.py
+++ plot_temperature.py
@@ -2,11 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('temperature_dataset.csv')
+
+df = df.sort_values(['date','time'], ascending=True).reset_index(drop=True)
 plt.figure(figsize=(10,5))
-plt.plot(df['time'], df['temperature_C'], label='Temperature (°C)')
-plt.xlabel('Time')
+plt.plot(df.index, df['temperature_C'], label='Temperature (°C)')
+plt.xlabel('Observation (hour)')
 plt.ylabel('Temperature (°C)')
 plt.title('Room temperature variation (1–7 Aug 2025)')
 plt.legend()
 plt.tight_layout()
-plt.savefig('results/temperature_plot.png')
+plt.show()
+
